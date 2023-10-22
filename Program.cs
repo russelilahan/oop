64853ee967390ffd3e0e6b33b798045d4e17aa97
russelilahan <144908004+russelilahan@users.noreply.github.com>
2023-10-22 14:19:44 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program{
+
+    static void Main (string [] args){
+try
+{
+    Console.WriteLine("Enter int number");
+    var input = Console.ReadLine();
+    int number = int.Parse(input);
+
+    if ( number < 0 )
+    {
+        throw new Exception ("Invalid Number");
+    }
+
+        double result = Math.Sqrt(number);
+        Console.WriteLine($"The square root is{result}");
+
+}
+catch (FormatException e)
+{
+        Console.WriteLine("Invalid number");
+        
+}
+    }
+}
